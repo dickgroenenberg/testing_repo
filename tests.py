88c8a95e78bb74edfde48ea9de3ce96522711129
dickgroenenberg <dickgroenenberg@gmail.com>
2015-04-23 08:39:03 +0200

    test.py added

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+
+import seq_utils
+
+
+class TestSeqUtils(unittest.TestCase):
+    def test_is_codon_correct(self):
+        codon = 'ACG'
+        result = seq_utils.is_codon_correct(codon)
+        expected = True
+        self.assertEqual(expected, result)
+        
+    def test_is_codon_correct_bad_codon_symbol(self):
+        codon = 'A$G'
+        result = seq_utils.is_codon_correct(codon)
+        expected = False
+        self.assertEqual(expected, result) 
+        
+    def test_is_codon_correct_bad_codon_number(self):
+        codon = 314
+        result = seq_utils.is_codon_correct(codon)
+        expected = False
+        self.assertEqual(expected, result) 
